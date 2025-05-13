b9dd1ef835b548084ff99302747909915e390b21
JoseDA0721 <133171655+JoseDA0721@users.noreply.github.com>
2025-05-13 17:58:09 -0500

    QuintoCommit-JavaScript y typeScript

---
+++ 00-servidor-web/assets/js/01-javascript.js
@@ -0,0 +1,12 @@
+console.log("Mundo");
+//Tipos de variables
+//Mutables
+let nombre = "Jose";
+nombre = "Daniel";
+// nombre = 1;
+// nombre = function(){
+//     return 1
+// };
+//Inmutables
+const cedula = 1727158303;
+// cedula = 2;
